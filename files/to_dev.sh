521e7936c8d6309d67725f5bf6a531e33b039b71
liaohuqiu <liaohuqiu@gmail.com>
2013-07-09 23:33:22 +0800

    add files

--- files/to_dev.sh
+++
@@ -1,9 +0,0 @@
-dir=`pwd`
-#!/bin/bash
-host="192.168.0.168"
-host="192.168.0.177"
-user="huqiu"
-password="UDZXYktMXD0ZAreRsOVBlIVvse"
-password="XEdM99xWUcRFKvwNUjh6OgkB6L"
-prePASSCODE="wwww"
-./auto_login.sh $host $user $password $prePASSCODE
--- files/to_product.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-host="123.125.56.100"
-user="huqiu"
-password="i0Q63W6O3TOOh18z32NWGnbaOuas"
-prePASSCODE="wwww"
-./auto_login.sh $host $user $password $prePASSCODE
